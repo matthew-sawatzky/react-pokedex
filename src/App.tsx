e293537860a1f8d788316ea64403645a7bdbc1cd
MatthewSawatz <msawatzky2@rrc.ca>
2024-10-24 17:33:20 -0500

    fonts and background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Background from "./components/Background";
+import Navbar from "./sections/Navbar";
+import Wrapper from "./sections/Wrapper";
+import Footer from "./sections/Footer";
+import "./scss/index.scss";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="main-container">
+      <Background />
+      <div className="app">
+        <Navbar />
+        <Wrapper />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
