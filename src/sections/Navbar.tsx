bfecd3cff382ed154606f358bf92faf9a84eda69
MatthewSawatz <msawatzky2@rrc.ca>
2024-10-25 08:25:02 -0500

    nav bar styling

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import pokeballIcon from "../assets/pokeball-icon.png";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <div className="block">
+        <img src={pokeballIcon} alt="Pokeball Icon" />
+      </div>
+      <div className="data"></div>
+      <div className="block"></div>
+      <GiHamburgerMenu />
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/sections/Wrapper.tsx
+++ src/sections/Wrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Wrapper() {
   return (
-    <div>Wrapper</div>
+    <div className="content">Wrapper</div>
   )
 }
 
