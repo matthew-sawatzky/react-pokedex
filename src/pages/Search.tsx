c891b55ddf86ea60b46e70221687f8562bf79120
MatthewSawatz <msawatzky2@rrc.ca>
2024-10-30 13:45:03 -0500

    rest api data implemented

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { useEffect } from 'react'
 import Wrapper from '../sections/Wrapper'
+import { useAppDispatch } from '../app/hooks'
+import { getInitialPokemonData } from '../app/reducers/getInitialPokemonData';
+import { useAppSelector } from '../app/hooks'
 
 function Search() {
+  const dispatch = useAppDispatch();
+  const { allPokemon } = useAppSelector(({pokemon}) => pokemon)
+  useEffect(() => {
+    dispatch(getInitialPokemonData())
+  }, [dispatch])
+
+  useEffect(() => {
+if(allPokemon){
+  const clonedPokemons = [...allPokemon]
+  const randomPokemonsId = clonedPokemons.sort(() => Math.random()-Math.random()).slice(0, 20);
+  console.log(randomPokemonsId);
+
+}
+  }, [allPokemon, dispatch])
   return (
     <div>Search</div>
   )
--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -1,3 +1,18 @@
 export interface AppTypeInitialState{
     
 }
+
+export interface PokemonTypeInitialState{
+    allPokemon: undefined | genericPokemonType[];
+}
+
+export interface genericPokemonType{
+    name: string;
+    url: string;
+}
+
+export interface generatedPokemonType{
+    name: string;
+    id: number;
+    image: string;
+}
