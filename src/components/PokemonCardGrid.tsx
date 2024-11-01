e7b5ab6ff7e9b402a01d21b1b5a852d70cf2918b
MatthewSawatz <msawatzky2@rrc.ca>
2024-10-31 21:58:18 -0500

    pokemon type images

--- src/components/PokemonCardGrid.tsx
+++ src/components/PokemonCardGrid.tsx
@@ -1,4 +1,4 @@
-import { userPokemonsType } from "../utils/Types";
+import { pokemonTypeInterface, userPokemonsType } from "../utils/Types";
 
 function PokemonCardGrid({ pokemons }: { pokemons: userPokemonsType[] }) {
   console.log(pokemons); // Add this line to log the pokemons array
@@ -19,7 +19,28 @@ function PokemonCardGrid({ pokemons }: { pokemons: userPokemonsType[] }) {
                   src={data.image}
                   alt="pokemon"
                   className="pokemon-card-image"
+                  loading="lazy"
                 />
+                <div className="pokemon-card-types">
+                  {data.types.map(
+                    (type: pokemonTypeInterface, index: number) => {
+                      const keys = Object.keys(type);
+                      return (
+                        <div className="pokemon-card-types-type" key={index}>
+                          <img
+                            src={type[keys[0]].image}
+                            alt="pokemon type"
+                            className="pokemon-card-types-type-image"
+                            loading="lazy"
+                          />
+                          <h6 className="pokemon-card-types-type-text">
+                            {keys[0]}
+                          </h6>
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
               </div>
             );
           })}
