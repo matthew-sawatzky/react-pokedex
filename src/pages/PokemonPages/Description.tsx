c90212ab016da275639ad00da50adb8ff9afdb89
MatthewSawatz <msawatzky2@rrc.ca>
2024-11-05 16:26:10 -0600

    pokemon styles

--- src/pages/PokemonPages/Description.tsx
+++ src/pages/PokemonPages/Description.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useAppSelector } from '../../app/hooks'
+import PokemonContainer from '../../components/PokemonContainer'
 
 function Description() {
+    const pokemonData = useAppSelector(
+    ({ pokemon: { currentPokemon } }) => currentPokemon
+    )
   return (
-    <div>Description</div>
+    <div>
+        <PokemonContainer image={pokemonData?.image!} />
+    </div>
   )
 }
 
