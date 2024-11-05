cef89e81f15652bcef27273a6bf0cadc756cdb03
MatthewSawatz <msawatzky2@rrc.ca>
2024-11-05 15:59:47 -0600

    capable moves added to pokemon page

--- src/pages/PokemonPages/CapableMoves.tsx
+++ src/pages/PokemonPages/CapableMoves.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useAppSelector } from "../../app/hooks";
 
 function CapableMoves() {
+  const pokemonData = useAppSelector(
+    ({ pokemon: { currentPokemon } }) => currentPokemon
+  );
   return (
-    <div>CapableMoves</div>
-  )
+    <div className="page capable-moves">
+      <h1 className="capable-moves-title">Abilities</h1>
+      <ul className="capable-moves-list ability">
+        {pokemonData?.pokemonAbilities.abilities.map((ability: string) => (
+          <li className="move" key={ability}>
+            {ability}
+          </li>
+        ))}
+      </ul>
+      <h1 className="capable-moves-title">Moves</h1>
+      <ul className="capable-moves-list">
+        {pokemonData?.pokemonAbilities.moves.map((ability: string) => (
+          <li className="move" key={ability}>
+            {ability}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default CapableMoves
+export default CapableMoves;
