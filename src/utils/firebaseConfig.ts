dcba6ba42772347b13681a013fc83e19f5e29997
MatthewSawatz <msawatzky2@rrc.ca>
2024-11-06 09:40:20 -0600

    firebaseconfig file added

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -3,6 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { collection } from "firebase/firestore";
 
+
 const FirebaseConfig = {
   apiKey: "AIzaSyDfhBrAdfd8r2DyxPl_ASmJVlhqyLlIHMM",
 
